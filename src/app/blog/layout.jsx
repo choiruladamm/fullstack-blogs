bb4b940e6a478572c2d6ede8c7c5a118d74e2ddf
choiruladamm <choiruladamm@gmail.com>
2023-06-04 21:02:02 +0700

    Setup route folder

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      <h1>Blog Layout</h1>
+      {children}
+    </div>
+  )
+}
+
+export default BlogLayout
