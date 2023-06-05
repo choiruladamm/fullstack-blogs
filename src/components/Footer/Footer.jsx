541da2cd534fdcc92340c902fb8022a10c2bd78c
choiruladamm <choiruladamm@gmail.com>
2023-06-05 20:42:22 +0700

    Styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className={styles.social}>
         <Image src="/1.png" width={15} height={15} alt="..." className={styles.icon} />
         <Image src="/2.png" width={15} height={15} alt="..." className={styles.icon} />
-        <Image src="/3.png" width={15} height={15} alt="..." className={styles.icon} />
+        <Image src="/3.png" width={15} height={15} alt="..." className={styles.icon} /> 
         <Image src="/4.png" width={15} height={15} alt="..." className={styles.icon} />
       </div>
     </div>
