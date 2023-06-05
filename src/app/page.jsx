11a22a0964eb2e3f2a68f23de0700bc82ec4cd6c
choiruladamm <choiruladamm@gmail.com>
2023-06-05 21:43:09 +0700

    Styling homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import Hero from 'public/hero.png'
-import styles from './page.module.css'
-import Button from '@/components/Button/Button'
+import Image from "next/image";
+import Hero from "public/hero.png";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
 
 const Home = () => {
   return (
@@ -11,15 +11,16 @@ const Home = () => {
           Better design for your digital products.
         </h1>
         <p className={styles.desc}>
-          Turning your Idea into Reality. We bring together the teams from the global tech industry.
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
         </p>
-        <Button url='/portofolio'>See Our Works</Button>
+        <Button url="/portofolio">See Our Works</Button>
       </div>
       <div className={styles.item}>
-        <Image src={Hero} alt='' className={styles.img}  />
+        <Image src={Hero} alt="" className={styles.img} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
