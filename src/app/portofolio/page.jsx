1d7142f6ac8109dc49cbb71fd93c7085cd00ad8b
choiruladamm <choiruladamm@gmail.com>
2023-06-06 14:06:56 +0700

    Styling portofolio

--- src/app/portofolio/page.jsx
+++ src/app/portofolio/page.jsx
@@ -7,13 +7,13 @@ const Portfolio = () => {
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
-        <Link href="/portfolio/illustrations" className={styles.item}>
+        <Link href="/portofolio/illustrations" className={styles.item}>
           <span className={styles.title}>Illustrations</span>
         </Link>
-        <Link href="/portfolio/websites" className={styles.item}>
+        <Link href="/portofolio/websites" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
-        <Link href="/portfolio/application" className={styles.item}>
+        <Link href="/portofolio/applications" className={styles.item}>
           <span className={styles.title}>Application</span>
         </Link>
       </div>
