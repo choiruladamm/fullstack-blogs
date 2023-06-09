5ee946eeef46a3f264591692788b774e47542f28
choiruladamm <choiruladamm@gmail.com>
2023-06-09 20:15:30 +0700

    Finish styling portofolio

--- src/app/portofolio/[category]/page.jsx
+++ src/app/portofolio/[category]/page.jsx
@@ -17,6 +17,7 @@ const getData = (cat) => {
 
 const Category = ({ params }) => {
   const data = getData(params.category);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
